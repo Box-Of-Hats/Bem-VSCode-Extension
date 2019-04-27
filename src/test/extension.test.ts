b708262c8ce283df4d5183fd1e6f096ef6c5cb0b
Box-Of-Hats <ja1ke@hotmail.co.uk>
2019-04-28 00:19:01 +0100

    formatting

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,17 +1,6 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
 import * as assert from "assert";
 import * as bemHelper from "../extension";
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-// import * as vscode from 'vscode';
-// import * as myExtension from '../extension';
 
-// Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", function() {
     // Defines a Mocha unit test
     test("CSS Class extraction - Multiple", function() {
@@ -98,7 +87,13 @@ suite("Extension Tests", function() {
 
     test("CSS Generation - Multiple Nested", function() {
         var actual = bemHelper.generateStyleSheet(
-            ["test-class", "class-test__element", "class-test", "class-test__element--one", "class-test__element--two"],
+            [
+                "test-class",
+                "class-test__element",
+                "class-test",
+                "class-test__element--one",
+                "class-test__element--two"
+            ],
             false
         );
         var expected = ".class-test{&__element{&--one{}&--two{}}}.test-class{}";
